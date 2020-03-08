2b5fd898d910ed4218109cbc896ad430293bb791
mesung <myhs2002@gmail.com>
2020-03-08 18:48:34 +0900

    1.5.3 스프링 IoC의 용어 정리

--- Ch01/ch01-toby/src/test/java/com/mesung/toby/ch01toby/dao/UserDaoTest.java
+++ Ch01/ch01-toby/src/test/java/com/mesung/toby/ch01toby/dao/UserDaoTest.java
@@ -9,6 +9,14 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String [] args) throws ClassNotFoundException, SQLException {
+
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao dao1 = daoFactory.userDao();
+        UserDao dao2 = daoFactory.userDao();
+
+        System.out.println(dao1);
+        System.out.println(dao2);
+
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao userDao = context.getBean("userDao", UserDao.class);
     }
